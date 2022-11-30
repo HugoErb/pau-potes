744932d3290ffade03fc476dd8462df35f6936d7
HugoErb <erb.hugo@yahoo.fr>
2022-11-30 21:43:03 +0100

    first commit

---
+++ js/form.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".js-input").keyup(function () {
+        if ($(this).val()) {
+            $(this).addClass("not-empty");
+        } else {
+            $(this).removeClass("not-empty");
+        }
+    });
+});
