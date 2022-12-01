942791db211aba3022c3fdf52a3ba1629dfabec4
Hugo Eribon <hugo.eribon@soprasteria.com>
2022-12-01 17:29:46 +0100

    fix coquille + minimize css + optimize files

--- js/form.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".js-input").keyup(function () {
-        if ($(this).val()) {
-            $(this).addClass("not-empty");
-        } else {
-            $(this).removeClass("not-empty");
-        }
-    });
-});
